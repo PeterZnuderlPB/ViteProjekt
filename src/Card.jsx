fc3ab01a4efb6db6ff0d370661ba34919439a0f6
PeterZnuderlPB <peter.znuderl@pro-bit.si>
2022-08-17 10:05:28 +0200

    Testing multiple state fileds

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,23 +2,27 @@ import { useState } from 'react'
 import './Card.css'
 
 function Card() {
-  //const [count, setCount] = useState(0)
+  const [followers, setFollowers] = useState(1234)
+  const [firstName, setFirstName] = useState("Jane")
+
+  const handleNameChange = event => {
+    setFirstName(event.target.value);
+  };
 
   return (
     <div className="card">
       <div className="card-background-img">
-
+        <img src="https://images.unsplash.com/photo-1574230737626-341cb22a191b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjI2ODc1NjE5&ixlib=rb-1.2.1&q=80&w=1080" className='card-background-img' alt='background'/>
       </div>
-      <div className='card-profile-img'>
-
-      </div>
+      <img src="/vite.svg" className="card-profile-img" alt="Profile image" />
       <div className='card-info-area'>
-        <h1 className='profile-name'>Jane Doe</h1>
+        <h1 className='profile-name'>{firstName} Doe</h1>
         <p className='profile-description'> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mattis massa vitae arcu vehicula iaculis. Nulla a massa nec dolor tincidunt porttitor eget ut leo.</p>
+
       </div>
       <div className='card-follow-info'>
         <div className='follow-info'>
-          <p className='card-bold-text'>1234</p>
+          <p className='card-bold-text'>{followers}</p>
           <p className='card-simple-text'>Followers</p>
         </div>
         <div className='follow-info'>
@@ -29,8 +33,9 @@ function Card() {
         </div>
       </div>
       <div className='card-bottom-button'>
-        <button className='success-button'>Follow</button>
+        <button className='success-button card-bottom-button' onClick={()=>setFollowers((followers)=>followers + 1)}>Follow</button>
       </div>
+      <input onChange={handleNameChange} placeholder='First Name'/>
     </div>
   )
 }
